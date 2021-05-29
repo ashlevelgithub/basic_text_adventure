99cc32be75ce0fe7b64768a4d60e6795f74ef4c2
ashlevelgithub <84883468+ashlevelgithub@users.noreply.github.com>
2021-05-29 03:17:19 -0400

    Update Text Based Adventure Game.py

--- Text Based Adventure Game.py
+++ Text Based Adventure Game.py
@@ -1,17 +1,18 @@
-
+#Program begins by defining a callback function dummy_proof_func that is called for incorrect user inputs throughout the program
+#Function displays a sarcastic message, and uses the exit command to exit and end the program 
 def dummy_proof_func():
     print ('''Ok smart alec that's not a proper response to the question, relaunch the program, my creator doesn't know loops yet,
 congrats you broke the code. ''')
     exit()
 
-
+#program then portrays the origin of the story, and asks the user if they want to play the game. Output is controlled with the if operator 
 print ("""Hello, and welcome to the Ashlands wanderer. This is a text based adventure game that will ask you to make 
           many decisions along the way, that will influence the way that you survive and prosper! """)
 print ("Beware though traveller, one wrong move, and it may be your last...")
 print ('Would you like to start the adventure? (key in "y" or "n" to answer this question and all further ones)')
 initial_question = input()
 
-
+#The program then gives the user 2 choices to advance the story, with a nested if command controlling the output and end of the program 
 if initial_question == "y":
     print ("And so, the adventure begins!")
     print ("Its nighttime, and you see 2 paths in front of you, the left one that leads into the woods, brambly and barren is the path.")
